d60f1671d8f5c5e0ddba6d8c399c3d3cb2428e26
Georgii Zagladin <georgii.zagladin@t-systems.com>
2019-06-26 23:54:24 +0300

    Updates

---
+++ precache-manifest.dd5f5a28c4f147ca158b7d1f741c2c01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c2555f5195a5f4833e3177065ca1d7c",
+    "url": "/zagladin/react-neon/static/media/wall.0c2555f5.jpg"
+  },
+  {
+    "revision": "d4d3f4e0ead46b1472c5104cee7fac59",
+    "url": "/zagladin/react-neon/static/media/tlgrm-round.d4d3f4e0.png"
+  },
+  {
+    "revision": "6976c96a4793fa60b9d370410bef647e",
+    "url": "/zagladin/react-neon/static/media/linked-round.6976c96a.png"
+  },
+  {
+    "revision": "2276d9a1e6cb0e9eba6333bbea4dc205",
+    "url": "/zagladin/react-neon/static/media/email.2276d9a1.png"
+  },
+  {
+    "revision": "80d05a3b71988d93027f",
+    "url": "/zagladin/react-neon/static/js/runtime~main.80d05a3b.js"
+  },
+  {
+    "revision": "825f80cae8b11c6ddb4f",
+    "url": "/zagladin/react-neon/static/js/main.825f80ca.chunk.js"
+  },
+  {
+    "revision": "049b34e0f0c27ebb2e7e",
+    "url": "/zagladin/react-neon/static/js/2.049b34e0.chunk.js"
+  },
+  {
+    "revision": "825f80cae8b11c6ddb4f",
+    "url": "/zagladin/react-neon/static/css/main.15bee099.chunk.css"
+  },
+  {
+    "revision": "e415d373e24949c84bd84f5a0026c604",
+    "url": "/zagladin/react-neon/index.html"
+  }
+];
