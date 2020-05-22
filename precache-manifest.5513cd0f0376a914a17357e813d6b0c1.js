e5069f38b7dd3fe3e63f7651a37b16a091b16135
Georgii Zagladin <georgii.zagladin@t-systems.com>
2020-05-22 16:55:16 +0300

    Updates

---
+++ precache-manifest.5513cd0f0376a914a17357e813d6b0c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4382f1eae392ad26999a0cd2dbde64b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39f70999b05322b09031",
+    "url": "/static/css/main.06599935.chunk.css"
+  },
+  {
+    "revision": "6ca0d9b4a1f92a49a29d",
+    "url": "/static/js/2.bc25801a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc25801a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f70999b05322b09031",
+    "url": "/static/js/main.51ab2b21.chunk.js"
+  },
+  {
+    "revision": "762f90a91cde268a7a70",
+    "url": "/static/js/runtime-main.7e703d23.js"
+  },
+  {
+    "revision": "4800f7e13f9c4f62a0b839310a56af36",
+    "url": "/static/media/neoneon-webfont.4800f7e1.woff2"
+  },
+  {
+    "revision": "51d7ef4651dffad5f928395f16c4128d",
+    "url": "/static/media/neoneon-webfont.51d7ef46.ttf"
+  },
+  {
+    "revision": "5a3a57ce30eb29dc35c34cbf73307a21",
+    "url": "/static/media/neoneon-webfont.5a3a57ce.woff"
+  },
+  {
+    "revision": "0c2555f5195a5f4833e3177065ca1d7c",
+    "url": "/static/media/wall.0c2555f5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a17856c806f2c053d68b58916983e527.js"
+  "/precache-manifest.5513cd0f0376a914a17357e813d6b0c1.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
